bbe8ef5e3015c5603af68c072b23b4e245fce7ce
Werner Eberling <werner.eberling@mathema.de>
2022-01-20 16:58:23 +0100

    Support opentracing.

--- src/main/java/de/mathema/sample/rest/TimeRestController.java
+++ src/main/java/de/mathema/sample/rest/TimeRestController.java
@@ -2,15 +2,17 @@ package de.mathema.sample.rest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TimeRestController {
 
     @GetMapping("/now")
-    public String getTime(){
+    public String getTime(@RequestHeader Map<String, String> headers){
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.YYYY - HH:mm:ss"));
     }
 
